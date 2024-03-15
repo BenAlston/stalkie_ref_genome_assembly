c214cee71a647e9cad9b4c3f895c1a480f6c16b5
BenAlston <159305266+BenAlston@users.noreply.github.com>
2024-03-15 14:11:48 +0000

    Update blast_par.sh

--- scripts/blast_par.sh
+++ scripts/blast_par.sh
@@ -1,29 +1,21 @@
+
 #!/bin/bash
 #SBATCH --job-name=blast
-#SBATCH --mem=10G
-#SBATCH -c 16
+#SBATCH --mem=6G
+#SBATCH -c 6
 #SBATCH --time=96:00:00
-#SBATCH -e reports/error_par_blast.txt
-#SBATCH -o reports/output_par_blast.txt
-#SBATCH --array 1-500%50 # concurrent job limit is 50, othewise i'd do 500
-
-#########################################################################################
-#      Script Name: blast_par
-#      Description: runs blast on a genome assembly subdivided into 500 smaller .fa files
-#      Author:      Ben Alston
-#      Date: March 2024
-#########################################################################################
+#SBATCH -e reports/blast/error_blast.txt
+#SBATCH -o reports/blast/output_blast.txt
+#SBATCH --array 1-500%50 # max jobs per user is 50
 
 wd=/mnt/parscratch/users/bip23bta/ref_genomes
-species=dalmanni
-assembly=6
-input_file=${assembly}_primary.fa
+species=whitei
+assembly=1
 
-cd $wd/$species/03-QC/blast
+cd $wd/$species/03-QC/blast/${assembly}_primary
 
-# using the apptainer image of blast since the other versions dont work
 apptainer exec ~/blast_latest.sif \
         blastn -db ${wd}/blast_nt_db/nt \
        -query split/${assembly}_primary.part-${SLURM_ARRAY_TASK_ID}.fa \
        -outfmt "6 qseqid staxids bitscore std" \
-       -out out/${SLURM_ARRAY_TASK_ID}_blast.out
+       -out blast_out/${SLURM_ARRAY_TASK_ID}_blast.out
