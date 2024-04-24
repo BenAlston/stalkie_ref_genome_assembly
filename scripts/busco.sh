4dabadf27ff7d5464172cf60253ba55a6ff527bf
BenAlston <159305266+BenAlston@users.noreply.github.com>
2024-04-24 15:03:37 +0100

    Update busco.sh

--- scripts/busco.sh
+++ scripts/busco.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=busco
+#SBATCH --mem=10G
+#SBATCH -c 20
+#SBATCH --time=24:00:00
+#SBATCH -e reports/error.%j_busco.txt
+#SBATCH -o reports/output.%j_busco.txt
+#SBATCH --array 0%1 # change depending on how many assemblies you're making, needs to start with 0
+
 #########################################################################################
 #	Script Name: busco.sh
 #	Description: busco assesment for hifiasm assemblies
