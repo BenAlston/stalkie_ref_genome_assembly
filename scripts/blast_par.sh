ec3b942b16d621b42973898393b9ee2ab55bd24d
BenAlston <159305266+BenAlston@users.noreply.github.com>
2024-04-24 14:36:01 +0100

    Update blast_par.sh

--- scripts/blast_par.sh
+++ scripts/blast_par.sh
@@ -7,6 +7,13 @@
 #SBATCH -o reports/blast/output_blast.txt
 #SBATCH --array 1-500%50 # max jobs per user is 50
 
+#########################################################################################
+#	Script Name: busco.sh
+#	Description: Runs blast on an assembly split into 500 smaller fasta files using fasta_splitter.sh, each containing numbers 1-500 in their filenames.
+#	Author:      Ben Alston
+#	Date:        April 2024
+#########################################################################################
+
 wd=/mnt/parscratch/users/bip23bta/ref_genomes
 species=whitei
 assembly=2
@@ -23,3 +30,7 @@ apptainer exec ~/blast_latest.sif \
        -max_hsps 1 \
        -evalue 1e-25 \
        -out blast_out/${SLURM_ARRAY_TASK_ID}_blast.out
+
+# If this script fails with an esoteric error about language localisation it is likely because you have forgotten to make the output directory specificed in the -out argument (/blast_out in this case)
+# this script also requires you manually to condense the 500 individual blast output files e.g:
+# cat blast_out/* > $assembly_name.blast.out
