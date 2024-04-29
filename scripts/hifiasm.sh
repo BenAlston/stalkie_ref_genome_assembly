2607395686dd047698e2311ddc520aebbccd8fa7
BenAlston <159305266+BenAlston@users.noreply.github.com>
2024-04-29 09:45:16 +0100

    Update hifiasm.sh
    made the script simpler, and not an array job, as this was unnecessarily complicated

--- scripts/hifiasm.sh
+++ scripts/hifiasm.sh
@@ -5,47 +5,38 @@
 #SBATCH --time=96:00:00
 #SBATCH -e reports/error%j_hifiasm.txt
 #SBATCH -o reports/output.%j_hifiasm.txt
-#SBATCH --array 0%2 # change depending on how many assemblies you're making, needs to start with 0
+
 
 #########################################################################################
 #	Script Name: hifiasm
-#	Description: genome assembly with hifiasm, seperate for M & F in each species (plus a whitei driver F)
+#	Description: genome assembly with hifiasm
 #	Author:      Ben Alston
-#	Date:        Feb 2024
+#	Date:        April 2024
 #########################################################################################
 
-# load package
+# uses Hifiasm v0.16.1-r375
 module load Anaconda3
 source activate hifiasm
 
 # Variables
-wd=/mnt/parscratch/users/bip23bta/ref_genomes
-species=whitei
-samples=(1 2 3)
-assembly=${samples[$SLURM_ARRAY_TASK_ID]}
-data=${wd}/$species/data/long_read/*${assembly}-Cell?/*.fastq.gz
-output_log=${wd}/$species/02-hifiasm/${SLURM_ARRAY_TASK_ID}_${assembly}.output.txt
-# these correspond to the liverpool prefixes, matched to our ones in <shared lab folder>/2024_Stalkie_PacBio_HiFI/>
+WD=/mnt/parscratch/users/bip23bta/ref_genomes
+SPECIES=dalmanni
+ASSEMBLY=7
+DATA=${WD}/$SPECIES/data/long_read/*${ASSEMBLY}-Cell?/*.fastq.gz
 
-# ------------ script
-cd ${wd}
+# ------------ script ------------ #
+cd ${WD}/$SPECIES/02-hifiasm
 
-# make and set dirs
-date
-echo making dirs and symlinks
-mkdir ${wd}/$species/02-hifiasm/${assembly}_hifiasm_output
+# make and set dirs -----------
+mkdir ${WD}/$SPECIES/02-hifiasm/${ASSEMBLY}_hifiasm_output
 
-cd ${wd}/$species/02-hifiasm/${assembly}_hifiasm_output
+cd ${WD}/$SPECIES/02-hifiasm/${ASSEMBLY}_hifiasm_output
 
-# symbolic links
+# symbolic links to input reads - for some reason it wont run if the input reads aren't in the wd
+bash -c "ln -s ${DATA} ./"
 
-bash -c "ln -s ${data} ./"
+# hifiasm ----------
+hifiasm -o ${SPECIES}_${ASSEMBLY}.asm -t 48 $DATA
 
-# hifiasm
-date
-echo starting hifiasm
-hifiasm -o ${species}_${assembly}.asm -t 48 *.fastq.gz
-date
-
-# convert gfa to fa
-awk '/^S/{print ">"$2"\n"$3}' ${species}_${assenbly}.asm.bp.p_ctg.gfa  | fold > ${assembly}_primary.fa
+# convert .gfa to .fa
+awk '/^S/{print ">"$2"\n"$3}' ${SPECIES}_${ASSEMBLY}.asm.bp.p_ctg.gfa  | fold > ${ASSEMBLY}_primary.fa
