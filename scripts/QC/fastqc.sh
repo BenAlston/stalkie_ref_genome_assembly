3372eaa4097ec005df3cd51f05cf28e071d0baae
BenAlston <159305266+BenAlston@users.noreply.github.com>
2024-06-07 12:02:13 +0100

    Create fastqc.sh]

---
+++ scripts/QC/fastqc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=fastqc
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=2
+#SBATCH --time=24:00:00
+#SBATCH -e reports/fastqc_error.txt
+#SBATCH -o reports/fastqc_output.txt
+
+#########################################################################################
+#       Script Name: fastqc
+#       Description: FastQC Quality Assessment for Raw Data
+#       Author:      Ben Alston
+#       Date:        Jan 2024
+#########################################################################################
+
+module load Anaconda3
+source activate qc
+
+READS=/mnt/parscratch/users/bip23bta/ref_genomes/whitei/data/short_read/Trimmed_trimmomatic/*.paired.fastq
+OUTPUT=/mnt/parscratch/users/bip23bta/ref_genomes/whitei/data/short_read/Trimmed_trimmomatic/
+ 
+cd /mnt/parscratch/users/bip23bta/ref_genomes 
+
+for FILE in $READS    
+ 
+do fastqc $FILE -o $OUTPUT 
+
+done
