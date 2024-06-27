43b30b5ae9a0ee4eef42aa0e0e00e161388ef257
BenAlston <159305266+BenAlston@users.noreply.github.com>
2024-06-27 12:11:23 +0100

    Update cov_calc_1.sh

--- scripts/sex_chr_id/cov_calc.sh
+++ scripts/sex_chr_id/cov_calc.sh
@@ -3,35 +3,41 @@
 #SBATCH --mem=16G
 #SBATCH -c 4
 #SBATCH --time=96:00:00
-#SBATCH -e reports/cov_calc.error.%J.txt
 #SBATCH -o reports/cov_calc.output.%J.txt
-
-#########################################################################################
-#       Script Name: cov_calc.sh
-#       Description: cov                                
-#       Author:      Ben Alston                                                 
-#       Date:        Jun 2024                                                    
-#########################################################################################
+#SBATCH --array 0-4%5 # change depending on how many tasks you need doing
 
 # load bedtools/2.26.0-0 samtools/1.3.1-0
 module load Anaconda3
 source activate cov_calc
 
-cd /mnt/parscratch/users/bip23bta/ref_genomes/whitei/04-sex_chromosomes/
+#########################################################################################
+#       Script Name: cov_calc_1.sh
+#       Description: First stage in calculating coverage across 5kb genomic windows.
+#                    Run as an array with a file of file name prefixes. Takes the .bam file 
+#                    following alignment and outputs a sorted, merged, .bed file
+#       Author:      Ben Alston
+#       Date:        Jun 2024
+#########################################################################################
+# set wd
+cd /mnt/parscratch/users/bip23bta/ref_genomes/whitei/04-sex_chromosomes/males
+
+# read in a list of file prefixes corresponding to the samples I want to map, where each line is a new sample
+FILEPREFIXES=/mnt/parscratch/users/bip23bta/ref_genomes/whitei/04-sex_chromosomes/males/male_fileprefixes.txt
+
+# read this as an array (a specific kind of unix object, kinda like vectors in R)
+readarray -t array < $FILEPREFIXES
+
+# Specify that each slurm array job corresponds to a different sample
+FILENAME="${array[$SLURM_ARRAY_TASK_ID]}"
+
+echo "Calculating Coverage for $FILENAME"
 
 # sort and index the .bam file
-samtools sort male_whitei.bam -o male_whitei_sorted.bam
-samtools index male_whitei_sorted.bam
+samtools sort ${FILENAME}.bam -o ${FILENAME}_sorted.bam
+samtools index ${FILENAME}_sorted.bam
 
 # generates .bed file from .bam
-bedtools bamtobed -i male_whitei_sorted.bam > male_whitei_sorted.bam.bed
+bedtools bamtobed -i ${FILENAME}_sorted.bam > ${FILENAME}_sorted.bam.bed
 
 # merges all features of the .bed file into one
-# will need to check if this is the right way of doing it
-bedtools merge -i male_whitei_sorted.bam.bed > male_whitei_sorted_merged.bed
-
-# makewindows
-bedtools makewindows -b male_whitei_sorted_merged.bed -w 5000 > male_whitei_windows.bed
-
-# counts the number of alignments that overlap with the intervals specified by the .bed file
-bedtools multicov -bams male_whitei_sorted.bam -bed male_whitei_windows.bed > male_cov.tsv
+bedtools merge -i ${FILENAME}_sorted.bam.bed > ${FILENAME}_sorted_merged.bed
