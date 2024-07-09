adb28706f34e831a3ac55257f7854a02a6e7a04c
BenAlston <159305266+BenAlston@users.noreply.github.com>
2024-07-09 16:34:24 +0100

    Update bowtie2_alignment.sh

--- scripts/sex_chr_id/bowtie2_alignment.sh
+++ scripts/sex_chr_id/bowtie2_alignment.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 #BATCH --job-name=bowtie2_align
 #SBATCH --mem=16G
-#SBATCH -c 8
+#SBATCH -c 32
 #SBATCH --time=96:00:00
-#SBATCH -e reports/bowtie_align.error.%j.txt
 #SBATCH -o reports/bowtie_align.output.%j.txt
+#SBATCH --array 0-4%5 # change depending on how many tasks you need doing
 
 #########################################################################################
 #       Script Name: bowtie2_align.sh
-#       Description: Aligns short reads to female ref, converting to a .bam file
+#       Description: Aligns illumina paired reads to a ref. Swap between males and females with find and replace
 #       Author:      Ben Alston
 #       Date:        May 2024
 #########################################################################################
 
-# load bowtie2/2.4.1 and samtools/1.13
+# load bowtie2/2.5.3 and samtools
 module load Anaconda3
 source activate bowtie_align
 
 WD=/mnt/parscratch/users/bip23bta/ref_genomes/
-READS_1=/mnt/parscratch/users/bip23bta/ref_genomes/whitei/data/short_read/Trimmed/female/*R1_001.fastq.gz
-READS_2=/mnt/parscratch/users/bip23bta/ref_genomes/whitei/data/short_read/Trimmed/female/*R2_001.fastq.gz
+FILEPREFIXES=/mnt/parscratch/users/bip23bta/ref_genomes/whitei/04-sex_chromosomes/females/female_fileprefixes.txt
+
+readarray -t array < $FILEPREFIXES # a file of line seperated file prefixes
+
+FILENAME="${array[$SLURM_ARRAY_TASK_ID]}"
+
+READS_1=/mnt/parscratch/users/bip23bta/ref_genomes/whitei/data/short_read/Trimmed/female/??-${FILENAME}*R1_001.fastq.gz
+READS_2=/mnt/parscratch/users/bip23bta/ref_genomes/whitei/data/short_read/Trimmed/female/??-${FILENAME}*R2_001.fastq.gz
 REF_INDEX=/mnt/parscratch/users/bip23bta/ref_genomes/whitei/04-sex_chromosomes/index_1/whitei_1
+
+echo "running bowtie2 on sample $FILENAME"
 
 cd $WD 
 
-bowtie2 --mp 10000 -x $REF_INDEX -1 $READS_1 -2 $READS_2 | samtools view -bS - > whitei/04-sex_chromosomes/female_whitei.bam 
+bowtie2 --threads 32 --mp 10000 -x $REF_INDEX -1 $READS_1 -2 $READS_2 | samtools view -bS - > whitei/04-sex_chromosomes/females/${FILENAME}.bam
 # high mismatch penalty (mp) effectivley removes mismatches
