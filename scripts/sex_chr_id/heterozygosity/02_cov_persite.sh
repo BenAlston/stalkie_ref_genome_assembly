1f7f924e3b0dff68cd16b4797cdcfb19530b5c3e
BenAlston <159305266+BenAlston@users.noreply.github.com>
2025-10-14 14:45:03 +0100

    Update 02_cov_persite.sh

--- scripts/sex_chr_id/heterozygosity/02_cov_persite.sh
+++ scripts/sex_chr_id/heterozygosity/02_cov_persite.sh
@@ -3,23 +3,23 @@
 #SBATCH --mem=20G
 #SBATCH -c 2
 #SBATCH --time=02:00:00
-#SBATCH -o reports/cov_persite.txt
+#SBATCH -o reports/persite_cov/cov_persite.txt
 
 #########################################################################################
-#       Script Name: 02_cov_persite.sh
-#       Description: calculate coverage per site across the genome for each sample
-#                    used in het calculations 
+#       Script Name: bed_windows.sh
+#       Description: make bed file of n kb windows, required by bedtools multicov, to specify windows to generate coverage across
 #       Author:      Ben Alston                                                 
 #       Date:        Sep 2025                                                    
 #########################################################################################
 
 # we need per site coverage for each sample (from less stringent mapped dal_7 bams)
-BAMS=/mnt/parscratch/users/bip23bta/ref_genomes/dalmanni/05-sex_chr_id/02_Y_ID/${1}_bams.txt
+BAMS=/mnt/parscratch/users/bip23bta/ref_genomes/dalmanni/05-sex_chr_id/02_Y_ID/snpdensity/het_dal_6/M_bams.txt
 # genome file of scaffold names and lengths
-WD=/mnt/parscratch/users/bip23bta/ref_genomes/dalmanni/05-sex_chr_id/02_Y_ID/snpdensity/persite_cov
+WD=/mnt/parscratch/users/bip23bta/ref_genomes/dalmanni/05-sex_chr_id/02_Y_ID/snpdensity/het_dal_6/persite_cov
 
 mkdir -p $WD 
 cd $WD 
+
 samtools='apptainer exec /mnt/parscratch/users/bip23bta/docker_images/samtools_1.21--h50ea8bc_0.sif samtools'
 $samtools depth -a -f $BAMS -H > ${1}_cov.tsv
 
